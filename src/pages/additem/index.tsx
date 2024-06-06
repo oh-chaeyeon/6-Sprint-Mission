4126e9fbf5b108980395d315fda7b04f985631aa
oh-chaeyeon <chaebread555@gmail.com>
2024-06-07 03:45:49 +0900

    feat : tag 기능 추가 및 삭제 기능 완료

--- src/pages/additem/index.tsx
+++ src/pages/additem/index.tsx
@@ -5,6 +5,9 @@ import xicon from "../../assets/item/ic_X.svg";
 
 export default function Additem() {
   const [imagePreview, setImagePreview] = useState<string | null>("");
+  const [tags, setTags] = useState<string[]>([]);
+  const [tagInput, setTagInput] = useState<string>("");
+
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,8 +30,30 @@ export default function Additem() {
     }
   };
 
+  const handleTagInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTagInput(e.target.value);
+  };
+
+  const handleTagKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && tagInput.trim() !== "") {
+      e.preventDefault();
+      setTags([...tags, tagInput.trim()]);
+      setTagInput("");
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleTagDelete = (index: number) => {
+    const updatedTags = [...tags];
+    updatedTags.splice(index, 1);
+    setTags(updatedTags);
+  };
+
   return (
-    <form className="additem-container">
+    <form className="additem-container" onSubmit={handleSubmit}>
       <div className="additem-top">
         <div className="additem-title">상품 등록하기</div>
         <button className="additem-title-btn">등록</button>
@@ -70,25 +95,67 @@ export default function Additem() {
         <label htmlFor="additem_name" className="additem-write">
           상품명
         </label>
-        <input type="text" className="additem-input" id="additem_name" />
+        <input
+          type="text"
+          className="additem-input"
+          id="additem_name"
+          placeholder="상품명을 입력해주세요"
+        />
       </div>
       <div className="additem-input-container">
         <label htmlFor="additem_intro" className="additem-write">
           상품 소개
         </label>
-        <input type="text" className="additem-input" id="additem_intro" />
+        <input
+          type="text"
+          className="additem-input"
+          id="additem_intro"
+          placeholder="상품 소개를 입력해주세요"
+        />
       </div>
       <div className="additem-input-container">
         <label htmlFor="additem_price" className="additem-write">
           판매가격
         </label>
-        <input type="text" className="additem-input" id="additem_price" />
+        <input
+          type="text"
+          className="additem-input"
+          id="additem_price"
+          placeholder="판매 가격을 입력해주세요"
+        />
       </div>
       <div className="additem-input-container">
         <label htmlFor="additem_tag" className="additem-write">
           태그
         </label>
-        <input type="text" className="additem-input" id="additem_tag" />
+        <input
+          type="text"
+          className="additem-input"
+          id="additem_tag"
+          placeholder="태그를 입력해주세요"
+          value={tagInput}
+          onChange={handleTagInput}
+          onKeyDown={(e) =>
+            handleTagKeyPress(e as React.KeyboardEvent<HTMLInputElement>)
+          }
+        />
+        <div className="tag-container">
+          {tags.map((tag, index) => (
+            <div key={index} className="input-tag">
+              #{tag}
+              <button
+                className="tag-ic-X-background"
+                onClick={() => handleTagDelete(index)}
+              >
+                <img
+                  className="tag-ic-X"
+                  src={xicon}
+                  alt="이미지 삭제 아이콘"
+                />
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </form>
   );
